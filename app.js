416159e3e346a2f45baa42aad23d67e983f06f43
rahmiddinasiydinov <rahmiddinasiydinov>
2021-09-30 11:41:43 +0500

    korzinka

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const elSelect = selectElem('.home__type');
 const elRoomInput = selectElem('.room__number');
 const elSearch = selectElem('.form__submit');
 const elTemplate = selectElem('#template').content;
+const elHeaderLike = selectElem('.header__like');
+let modal = selectElem('.modal');
+let modalExit = selectElem('.modal__exit', modal);
+let modalList = selectElem('.modal__list', modal);
+
 
 function renderHomes(homesArr, element){
     element.innerHTML = null;
@@ -25,11 +30,45 @@ function renderHomes(homesArr, element){
         selectElem('.home__time', cloneTemplate).textContent = normalizeDate(home.release_date);
         selectElem('.home__time', cloneTemplate).datetime = normalizeDate(home.release_date);
         selectElem('.films__btn', cloneTemplate);
+        selectElem('.home__like', cloneTemplate).dataset.id = home.id
         
         element.appendChild(cloneTemplate);
     })
 }
 renderHomes(homes, elMenu);
+
+
+
+//render for like
+
+function renderLikes(homesArr, element){
+    element.innerHTML = null;
+    homesArr.forEach((home) =>{
+        const cloneTemplate = elTemplate.cloneNode(true);
+        
+        selectElem('.home__img', cloneTemplate).src = home.img;
+        selectElem('.home__title', cloneTemplate).textContent = home.title;
+        selectElem('.home__rooms', cloneTemplate).textContent = "Xonalar soni: " + home.rooms;
+        selectElem('.home__area', cloneTemplate).textContent = "Maydoni: " + home.area;
+        selectElem('.home__price', cloneTemplate).textContent = "Narxi: " + home.price + " $";
+        let elTypes = selectElem('.home__turi', cloneTemplate)
+        home.type.forEach((tip) =>{
+            let newLi = createElem('li');
+            newLi.textContent = tip;
+            elTypes.appendChild(newLi);
+        })
+        selectElem('.home__time', cloneTemplate).textContent = normalizeDate(home.release_date);
+        selectElem('.home__time', cloneTemplate).datetime = normalizeDate(home.release_date);
+        selectElem('.films__btn', cloneTemplate);
+        selectElem('.home__like', cloneTemplate).dataset.id = home.id
+        selectElem('.home__like',cloneTemplate).innerHTML='<i class="fas fa-trash"></i>' ;
+        selectElem('.home__like',cloneTemplate).classList = ('trash');
+        
+        element.appendChild(cloneTemplate);
+    })
+}
+
+
 
 function renderTypes(typeArr, element){
     let result = []
@@ -125,3 +164,93 @@ elForm.addEventListener('submit', (e) =>{
     elRoomInput.value = null;
     renderMovies(foundHomes, elMenu);
 })
+
+
+
+ // adding to likes
+let likeContainer = [];
+let likes = document.querySelectorAll('.home__like');
+likes.forEach(elem =>{
+    elem.addEventListener('click', ()=>{
+        
+        elem.classList.toggle('active');
+        console.log(elem.dataset.id);
+        console.log(elem.classList.contains('active'))
+        if(elem.classList.contains('active')){
+            homes.forEach(
+                el =>{
+                    if(el.id == elem.dataset.id){
+                        likeContainer.push(el);
+                    }
+                }
+            ); 
+           
+        }
+        else{
+            homes.forEach(
+                el =>{
+                    if(el.id == elem.dataset.id){
+                        likeContainer.forEach(like =>{
+                            if(like.id == el.id){
+                                let index = likeContainer.indexOf(like)
+                                likeContainer.splice(index,1)
+                            } 
+                        })
+                        
+                    }
+                }
+            );
+
+        }
+         renderLikes(likeContainer, modalList) 
+        }
+        )
+})
+
+//modal
+elHeaderLike.addEventListener('click',()=>{
+    modal.classList.toggle('active__modal');
+trash = modal.querySelectorAll('.trash')
+    console.log(trash);
+    trash.forEach((elem)=>{
+        elem.addEventListener('click' ,()=>{
+            console.log('hello')
+            homes.forEach(
+                el =>{
+                    if(el.id == elem.dataset.id){
+                        likeContainer.forEach(like =>{
+                            if(like.id == el.id){
+                                let index = likeContainer.indexOf(like)
+                                likeContainer.splice(index,1);
+                                
+
+                            } 
+                        })
+                        
+                    }
+                }
+            );
+            renderLikes(likeContainer, modalList) 
+        })
+    })
+   
+
+
+
+})
+modalExit.addEventListener('click', (elem)=>{
+    modal.classList.remove('active__modal')
+})
+
+const cloneTemplate = elTemplate.cloneNode(true)
+
+
+
+
+
+
+
+
+
+
+
