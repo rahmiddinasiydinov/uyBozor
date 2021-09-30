fa2e43a830f25ec125b33b66a13ea7deceea0343
rahmiddinasiydinov <rahmiddinasiydinov>
2021-09-30 17:14:43 +0500

    com

--- app.js
+++ app.js
@@ -242,20 +242,6 @@ likes.forEach(elem =>{
 //modal
 elHeaderLike.addEventListener('click',()=>{
     modal.classList.toggle('active__modal');
-
-   
-
-
-
-})
-modalExit.addEventListener('click', (elem)=>{
-    modal.classList.remove('active__modal')
-})
-
-const cloneTemplate = elTemplate.cloneNode(true)
-
-
-
 trash = modal.querySelectorAll('.trash')
     console.log(trash);
     trash.forEach((elem)=>{
@@ -284,6 +270,20 @@ trash = modal.querySelectorAll('.trash')
             renderLikes(likeContainer, modalList) 
         })
     })
+   
+
+
+
+})
+modalExit.addEventListener('click', (elem)=>{
+    modal.classList.remove('active__modal')
+})
+
+const cloneTemplate = elTemplate.cloneNode(true)
+
+
+
+
 
 
 
